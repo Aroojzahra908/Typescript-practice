50e8a78209beb41879c632f4c3364c8f2b2dba68
AROOJ ZAHRA <136663418+Aroojzahra908@users.noreply.github.com>
2023-08-25 21:20:45 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,65 @@
+"use strict";
+// Question no 1:
+// Write a typescript to check whether an `input` is an array or not.
+var array1 = [11, 22, 33, 44];
+console.log(Array.isArray(array1)); // Output: true
+//is not
+var array1 = [11, 22, 33, 44];
+console.log(!Array.isArray(array1)); //output false
+// for number 
+var a = 11;
+console.log(typeof (a));
+// Question no 2:
+// Write a JavaScript program to sort the items of an array.
+// Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+var arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+arr1.sort();
+console.log("the sorted array is:", arr1);
+// Question no 3:
+// Write a TypeScript program to find the most frequent item in an array.
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var arr2 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var newarray = Array.from(new Set(arr2));
+console.log(newarray);
+// Question no 4:
+// Write a typeScript program to compute the sum and product of an array of integers.
+var array3 = [1, 3, 4, 6, 7, 8, 4];
+var sum = 0;
+var product = 1; // 1*2 that's why we initialize by 1
+for (var i = 0; i < array3.length; i++) {
+    sum = sum + array3[i];
+    product *= array3[i];
+    // product=product*array3[i];
+}
+console.log("the sum is", sum, "and the product is", product);
+//Question no 5:
+// There are two arrays with individual values. Write a JavaScript program 
+// to compute the sum of each individual index value in the given array.
+var aa = [1, 0, 2, 3, 4];
+var bb = [3, 5, 6, 7, 8, 13];
+var minvalue = Math.min(aa.length, bb.length);
+var resultant_array = [];
+for (var i = 0; i < minvalue; i++) {
+    resultant_array.push(aa[i] + bb[i]);
+}
+console.log("the sum of two arrays is", resultant_array);
+//Question no 6:
+// There are two arrays with individual values. Write a JavaScript program to concatenate two arrays of each individual index value in the given
+// array.
+//method : for concatenate two arrays:
+var aaa = [1, 0, 2, 3, 4];
+var bbb = [3, 5, 6, 7, 8, 13];
+var result = aaa.concat(bbb);
+console.log(result); // Output: [1, 0, 2, 3, 4, 3, 5, 6, 7, 8, 13]
+//Question no 7:
+// Write a JavaScript program to find duplicate values in a typeScript array.
+var arr2 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var duplicates = [];
+var uniqueDuplicates = [];
+for (var i = 0; i < arr2.length; i++) {
+    if (arr2.indexOf(arr2[i]) !== i && duplicates.indexOf(arr2[i]) === -1) {
+        duplicates.push(arr2[i]);
+        uniqueDuplicates.push(arr2[i]);
+    }
+}
+console.log(uniqueDuplicates);
---
+++ functions_50.js
@@ -0,0 +1,148 @@
+"use strict";
+// Question no1:
+// Write a JavaScript function that reverses a number.
+function reverseNumber(value) {
+    return parseInt(value.toString().split('').reverse().join(''));
+}
+const originalNumber = 1233;
+const reversed = reverseNumber(originalNumber);
+console.log("Original number:", originalNumber);
+console.log("Reversed number:", reversed);
+// Question no 2:
+//Write a function that takes the length and width of a rectangle as parameters and calculates its area and perimeter.
+function calculate_area_parameter(length = 5, width = 10) {
+    var area = length * width;
+    var parameter = 2 * (length + width);
+    console.log("the area of a rectagle is:", area);
+    console.log("the parameter of a rectangle is:", parameter);
+}
+calculate_area_parameter();
+// Question no 3:
+//Create a function to convert temperatures between Celsius and Fahrenheit. Write two functions, one for each conversion direction.
+function ctof(celcius) {
+    var c_to_F = celcius * (9 / 5) + 32;
+    console.log("celcius to fahrenheit is:", c_to_F);
+    return c_to_F;
+}
+ctof(33);
+function ftoc(Fahrenheit) {
+    var f_to_c = (Fahrenheit - 32) * 5 / 9;
+    console.log("fahrenheit to celcius is:", f_to_c);
+    return f_to_c;
+}
+ftoc(78);
+// Question no 3:
+// Implement a function to calculate the factorial of a given positive integer?
+function factorial_is(fact) {
+    if (fact === 0 || fact === 1) {
+        return 1;
+    }
+    else {
+        return fact * factorial_is(fact - 1);
+    }
+}
+console.log("factorail is :", factorial_is(5));
+// Question no 3:
+// Write a function that checks whether a given number is positive or not.
+function prime(num1) {
+    if (num1 >= 0) {
+        console.log("the number is positive");
+    }
+    else {
+        console.log("the number is nagative");
+    }
+    return num1;
+}
+prime(-1);
+// Question no 4:
+// Write a typescript function to check whether an `input` is an array or not.
+//false
+function checkArray(input) {
+    return Array.isArray(input);
+}
+var a = 44;
+console.log(checkArray(a)); // Output: false
+//true
+function checkArray1(input) {
+    return Array.isArray(input);
+}
+var array = [44, 33, 44, 55];
+console.log(checkArray1(array)); // Output: true
+// Question no 5:
+// Write a JavaScript program that accepts a string as input and swaps the case of each character. 
+// For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+function swapCase(input) {
+    let result = '';
+    for (var i = 0; i < input.length; i++) {
+        var char = input[i];
+        if (char === char.toUpperCase()) {
+            result += char.toLowerCase();
+        }
+        else {
+            result += char.toUpperCase();
+        }
+    }
+    return result;
+}
+const inputString = 'The Quick Brown Fox';
+const swappedString = swapCase(inputString);
+console.log(swappedString); // Output: 'tHE qUICK bROWN fOX'
+// Question no 6:
+// Write a typescriptScript program that accepts a string as input and swaps the case of each character. 
+// For example if you input 'the QUiCk BroWn FoX' the output should be 'THE quIcK bRoWn fOx'.
+function swapcase2(input2) {
+    var result2 = '';
+    for (var i = 0; i < input2.length; i++) {
+        var char2 = input2[i];
+        if (char2 === char2.toUpperCase()) {
+            result2 = result2 + char2.toLowerCase();
+        }
+        else {
+            result2 = result2 + char2.toUpperCase();
+        }
+    }
+    return result2;
+}
+var inputstring = "the QUiCk BroWn FoX";
+var swapstring = swapcase2(inputstring);
+console.log(swapstring);
+// Question no 7:
+//  Write a typeScript program to find the leap years in a given range of years.
+function leapyear(startyear, endyear) {
+    var resultyear = [];
+    for (var year = startyear; year <= endyear; year++) {
+        if (year % 4 === 0) {
+            console.log("the given year is leap year");
+            resultyear.push(year);
+        }
+    }
+    return resultyear;
+}
+var startyear = 2000;
+var endyear = 2023;
+var leapyear_in_range = leapyear(startyear, endyear);
+console.log("Leap years between", startyear, "and", endyear, ":", leapyear_in_range);
+//Question no 8:
+// Write a JavaScript function that merges two arrays and removes all duplicate elements.
+function summ_of(arr1, arr22) {
+    var mergedArray = arr1.concat(arr22);
+    var unique_array = Array.from(new Set(mergedArray));
+    return unique_array;
+}
+var arr1 = [1, 2, 3, 4, 5];
+var arr22 = [2, 3, 4, 66, 7, 8];
+var result2 = summ_of(arr1, arr22);
+console.log(result2);
+//Question no 9:
+// Write a JavaScript function to remove a specific element from an array.
+var input = [11, 22, 33, 44, 55];
+var removed_number = input.splice(2, 1);
+console.log("Removed number:", removed_number);
+console.log("Remaining array:", input);
+function element(input) {
+    var removed_number = input.splice(2, 1);
+    return removed_number;
+}
+var input = [11, 22, 44, 66, 77];
+var final_array = element(input);
+console.log(final_array);
