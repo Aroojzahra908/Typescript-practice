91ae2b331c5d3b55f7fab437f81f90a9b6a277e7
AROOJ ZAHRA <136663418+Aroojzahra908@users.noreply.github.com>
2023-09-22 22:47:04 +0500

    Add files via upload

---
+++ setinterval.ts
@@ -0,0 +1,36 @@
+// async and wait 
+async function hello(){
+    if (5>3){
+        console.log("first")
+    }
+
+await console.log("in function")
+}
+hello()
+console.log("line 1")
+console.log("line 2")
+
+// setTimeout(function,time)
+
+console.log("before asyn function")
+function dosomething(){
+    console.log("print something")
+}
+setTimeout(()=>{
+    console.log("after asyn function")
+},5000)   //1000=1ms
+console.log("end")
+
+// set interval
+function _hello(){
+console.log("is set timee out 5");
+}
+setInterval(_hello,7000)
+console.log("after asyn function");
+console.log("line 2")
+
+//clear interval:
+const handle = setInterval((): void => {
+    console.log('This will be displayed every 1000ms (1s).');
+  }, 1000);
+clearInterval(handle);             // stops intevel
