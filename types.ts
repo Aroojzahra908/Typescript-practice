1177dd244d28906f6469541bcef7b6762143140c
AROOJ ZAHRA <136663418+Aroojzahra908@users.noreply.github.com>
2023-09-26 23:20:10 +0500

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,29 @@
+type student={
+    name:string,
+    email:string,
+}
+function sortedstudent<T>(arr:T[]):T[] {
+return arr
+}
+// ????
+
+// interface:
+interface student1{
+    rollno:number,
+    uni:string,
+
+}
+type A= number | null   //define type 
+// type is used for scaler type >>> contain = sign 
+
+// interface is not used for scaler type>>>>>>interface only for objects>>> and type
+//  for both interface and objects>>>> interface has no = sign 
+
+
+
+var anyvar:number=234
+let newObj:student1={
+    rollno:234,
+    uni:"ntu",
+} 
+
