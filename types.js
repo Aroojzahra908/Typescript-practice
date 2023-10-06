b539d1ff8c3595f26ff95019cec2c67619a66d01
AROOJ ZAHRA <136663418+Aroojzahra908@users.noreply.github.com>
2023-10-06 21:35:23 +0500

    Add files via upload

---
+++ types.js
@@ -0,0 +1,12 @@
+"use strict";
+function sortedstudent(arr) {
+    return arr;
+}
+// type is used for scaler type >>> contain = sign 
+// interface is not used for scaler type>>>>>>interface only for objects>>> and type
+//  for both interface and objects>>>> interface has no = sign 
+var anyvar = 234;
+let newObj = {
+    rollno: 234,
+    uni: "ntu",
+};
