73c4198761be290566173cd3462c95aeb7fca09b
AROOJ ZAHRA <136663418+Aroojzahra908@users.noreply.github.com>
2023-11-16 23:12:07 +0500

    Add files via upload

---
+++ assignment_6.ts
@@ -0,0 +1,76 @@
+// 1. Write a program that uses filter to remove all negative numbers from an array of numbers
+
+var numbers = [12, -45, 7, -89, 34, -67, 23, 56];
+var positiveNumbers = numbers.filter((num1:number)=> {
+    return num1 >= 0;
+});
+console.log("Original array:", numbers);
+console.log("Array after removing negative numbers:", positiveNumbers);
+
+
+// 2. Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2:
+
+var numbers = [1, 2, 3, 4, 5];
+var multipliedNumbers = numbers.map((num2:number)=> {
+    return num2 * 2;
+});
+console.log("Original array:", numbers);
+console.log("New array with multiplied numbers:", multipliedNumbers);
+
+// 3. Given an array of strings ["apple", "banana", "cherry", "date","grape"], use the filter method to create a new array containing 
+// only the fruits with more than 5 characters.
+
+var fruits:string[] = ["apple", "banana", "cherry", "date", "grape"];
+var longFruits = fruits.filter((fruit:any)=> {
+    return fruit.length > 5;
+});
+console.log("Original array:", fruits);
+console.log("New array with long fruits:", longFruits);
+
+// 4. Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the 
+// squares of even numbers.
+
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var evenSquaredNumbers = nums.filter((numb:number)=> {
+    return numb % 2 === 0              // Filter even numbers
+})
+    var arr2:number[]= nums.map((evenNumber:number) =>{
+    return evenNumber **2;       // Map to their squares
+});
+console.log("Original array:", nums);
+console.log("New array with squares of even numbers:", arr2);
+
+// Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is 
+// converted to Fahrenheit using the formula (Celsius * 9/5) + 32.
+
+var celsiusTemperatures = [0, 10, 20, 30, 40];
+var fahrenheitTemperatures = celsiusTemperatures.map((celsius)=>{
+    return (celsius * 9/5) + 32;
+});
+console.log("Original Celsius temperatures:", celsiusTemperatures);
+console.log("Fahrenheit temperatures:", fahrenheitTemperatures);
+
+// 6. Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods together to create a new array containing the 
+// doubled values of odd numbers.
+
+var numbers = [3, 6, 9, 12, 15, 18];
+
+var doubledOddValues = numbers.filter((num2)=> {
+    return num2% 2 !== 0; // Filter odd numbers
+})
+var ar4:number[]=numbers.map((oddNumber)=> {
+    return oddNumber * 2; // Map to their doubled values
+});
+console.log("Original array:", numbers);
+console.log("New array with doubled values of odd numbers:", doubledOddValues);
+
+// 7. Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an 
+// exclamation mark at the end, e.g., "Alice!"
+
+var names = ["Alice", "Bob", "Charlie", "David", "Emily"];
+names.forEach((name)=> {
+    console.log(name + "!");
+});
+
+
+
